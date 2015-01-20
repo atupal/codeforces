9efdf95f9479f97e2dca5ae245df443baceac2c4
atupal <atupalykl@gmail.com>
2015-01-20 00:55:35 +0000

    dfs to iter

--- contests/286_DIV2/c.cpp
+++ contests/286_DIV2/c.cpp
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <map>
+#include <vector>
+#include <algorithm>
+
 
 const int maxn = 30001;
 
 int sum[maxn];
 int cnt[maxn];
+std::vector<std::pair<int, int> > dp[maxn];
 
 int max;
 void dfs(int pos, int l, int ans) {
@@ -47,8 +52,36 @@ int main() {
     sum[i] = sum[i-1] + cnt[i];
   }
   max = 0;
-  dfs(0, d, 0);
+  //dfs(0, d, 0);
+  // no dfs, use iter!
+  dp[d].push_back(std::make_pair(d, cnt[d]));
+
+  for (int i = d; i < maxn; ++ i) {
+    int size = dp[i].size();
+    std::sort(dp[i].begin(), dp[i].end());
+    for (int j = size-1; j>=0; -- j) {
+      if ( j<size-1 && dp[i][j+1].first == dp[i][j].first ) {
+        continue;
+      }
+      int l = dp[i][j].first;
+      int s = dp[i][j].second;
+      if (s>max) {
+        max=s;
+      }
+      if (l-1 && i+l-1<maxn) {
+        dp[i+l-1].push_back(std::make_pair(l-1, s+cnt[i+l-1]));
+      }
+      if (l&&i+l<maxn) {
+        dp[i+l].push_back(std::make_pair(l, s+cnt[i+l]));
+      }
+      if (i+l+1<maxn) {
+        dp[i+l+1].push_back(std::make_pair(l+1, s+cnt[i+l+1]));
+      }
+    }
+  }
+
   printf("%d\n", max);
 
   return 0;
 }
+
