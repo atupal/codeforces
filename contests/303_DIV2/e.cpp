4e8a831a2df4b85397ef043cb3e920a403049654
atupal <atupalykl@gmail.com>
2015-05-20 14:50:49 +0000

    update

--- contests/303_DIV2/e.cpp
+++ contests/303_DIV2/e.cpp
@@ -10,20 +10,25 @@
 using namespace std;
 typedef long long ll;
 
-typedef pair<ll, ll> P;
+typedef pair<int, int> P;
+
+struct edge {
+  int v;
+  int idx;
+  int w;
+};
 
 const ll INF = 1LL<<61;
 const int maxn = (int) 3e5+10;
 int n, m, root;
-unordered_map<int, ll> w[maxn];
-unordered_map<int, int> idx[maxn];
-vector<int> out;
+vector<edge> g[maxn];
 ll ans;
-char vis[maxn];
 ll d[maxn];
+int w[maxn];
+int idx[maxn];
 
 void dijkstra() {
-  priority_queue<P, vector<P>, greater<P> > que;  
+  priority_queue<P, vector<P>, greater<P> > que;
   for (int i = 1; i <= n; ++ i) {
     d[i] = INF;
   }
@@ -35,11 +40,21 @@ void dijkstra() {
     que.pop();
     int v = p.second;
     if(d[v] < p.first) continue;
-    for (auto it=w[v].begin(); it != w[v].end(); ++ it) {
-      if(d[it->first] > d[v] + it->second) {
-        d[it->first] = d[v] + it->second;
-        que.push(P(d[it->first],it->first));
-        ans += it->second;
+    for (auto it=g[v].begin(); it != g[v].end(); ++ it) {
+      if(d[it->v] >= d[v] + it->w) {
+
+        if (d[it->v] == d[v] + it->w) {
+          if (w[it->v] > it->w) {
+            w[it->v] = it->w;
+            idx[it->v] = it->idx;
+          }
+        } else {
+          w[it->v] = it->w;
+          idx[it->v] = it->idx;
+        }
+
+        d[it->v] = d[v] + it->w;
+        que.push(P(d[it->v],it->v));
       }
     }
   }
@@ -47,55 +62,27 @@ void dijkstra() {
 }
 
 
-void dfs(int u, ll l) {
-  vector<P> tmp;
-  for (auto it = w[u].begin(); it != w[u].end(); ++ it) {
-    tmp.push_back( make_pair(it->second, it->first) );
-  }
-  sort(tmp.begin(), tmp.end());
-
-  //for (auto it = w[u].begin(); it != w[u].end(); ++ it) {
-  for (auto i = tmp.begin(); i != tmp.end(); ++ i) {
-    P *it = &(*i);
-    it->first ^= it->second ^= it->first ^= it->second;
-
-    if (vis[it->first]) continue;
-
-    if (l + it->second <= d[it->first]) {
-      vis[it->first] = 1;
-      ans += it->second;
-      out.push_back( idx[u][it->first] );
-      dfs(it->first, l+it->second);
-    }
-
-  }
-
-}
-
 void solve() {
   ans = 0;
   dijkstra();
-  /*
-  memset(vis, 0, sizeof(vis));
-  vis[root] = 1;
-  dfs(root, 0);
-  */
+  for (int i = 1; i <= n; ++ i) if (i != root) {
+    ans += w[i];
+  }
   cout << ans << endl;
-  for (auto it=out.begin(); it!=out.end(); ++ it) {
-    cout << *it << " ";
+  for (int i = 1; i <= n; ++ i) if (i != root) {
+    cout << idx[i] << " ";
   }
 }
 
 int main() {
+
   scanf("%d %d", &n, &m);
 
-  for (ll i = 0; i < m; ++ i) {
+  for (int i = 0; i < m; ++ i) {
     int x, y, W;
     scanf("%d %d %d", &x, &y, &W);
-    w[x][y] = (ll)W;
-    w[y][x] = (ll)W;
-    idx[x][y] = i+1;
-    idx[y][x] = i+1;
+    g[x].push_back( (edge) {y, i+1, W});
+    g[y].push_back( (edge) {x, i+1, W});
   }
 
   scanf("%d", &root);
